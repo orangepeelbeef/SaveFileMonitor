ff2f59a47bb2be5d493829cfbcdce5b4742a8e47
orangepeelbeef <130179+orangepeelbeef@users.noreply.github.com>
2024-05-21 13:21:46 -0700

    initial savefilemonitor application

---
+++ SaveFileMonitor/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SaveFileMonitor
+{
+    partial class save_file_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSelectFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(53, 165);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(112, 39);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(53, 231);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(82, 38);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(49, 310);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(120, 20);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Program Status";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSelectFile
+            // 
+            this.lblSelectFile.AutoSize = true;
+            this.lblSelectFile.Location = new System.Drawing.Point(181, 174);
+            this.lblSelectFile.Name = "lblSelectFile";
+            this.lblSelectFile.Size = new System.Drawing.Size(125, 20);
+            this.lblSelectFile.TabIndex = 4;
+            this.lblSelectFile.Text = "No File Selected";
+            // 
+            // save_file_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSelectFile);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "save_file_form";
+            this.Text = "Save File Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSelectFile;
+    }
+}
+
---
+++ SaveFileMonitor/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SaveFileMonitor
+{
+    public partial class save_file_form : Form
+    {
+        private FileSystemWatcher fileWatcher;
+        private string selectedFilePath;
+        private bool isChangeHandled = true; // Flag to track if change is being handled
+
+        public save_file_form()
+        {
+            InitializeComponent();
+            fileWatcher = new FileSystemWatcher();
+            fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            fileWatcher.Changed += FileChanged;
+        }
+
+        private void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            if (!isChangeHandled)
+                return;
+
+            // Set the flag to false to indicate that change is being handled
+            isChangeHandled = false;
+
+            // Create a timestamp for the copy file name
+            string timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            string fileName = Path.GetFileNameWithoutExtension(selectedFilePath);
+            string extension = Path.GetExtension(selectedFilePath);
+            string copyFilePath = Path.Combine(Path.GetDirectoryName(selectedFilePath), $"{fileName}_{timeStamp}{extension}");
+
+            // Copy the file to the new location
+            File.Copy(selectedFilePath, copyFilePath);
+
+            // Display status message
+            UpdateStatus($"File {Path.GetFileName(selectedFilePath)} has been changed. Copy created: {copyFilePath}");
+
+            // Start the timer
+            timer1.Start();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Files|*.*";
+            openFileDialog.Title = "Select a File to Monitor";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePath = openFileDialog.FileName;
+                lblSelectFile.Text = selectedFilePath;
+
+                // Begin monitoring the selected file
+                fileWatcher.Path = Path.GetDirectoryName(selectedFilePath);
+                fileWatcher.Filter = Path.GetFileName(selectedFilePath);
+                fileWatcher.EnableRaisingEvents = true;
+
+                // Display status message
+                UpdateStatus($"Now monitoring file: {selectedFilePath}");
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            // Close the application
+            Application.Exit();
+        }
+
+        private void UpdateStatus(string message)
+        {
+            // Update the status label text
+            lblStatus.Text = message;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Reset the flag to indicate that change handling is complete
+            isChangeHandled = true;
+
+            // Stop the timer
+            timer1.Stop();
+        }
+
+    }
+}
